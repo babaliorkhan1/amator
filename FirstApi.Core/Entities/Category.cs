c0cc6f40d7cf6d695e5a05cd0952caa3ee785d6e
Orxan <orkhanvb@code.edu.az>
2024-03-06 21:00:43 +0400

    budefe

--- FirstApi.Core/Entities/Category.cs
+++ FirstApi.Core/Entities/Category.cs
@@ -7,6 +7,5 @@ namespace FirstApi.Core.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsDeleted { get; set; }
-        //salam
     }
 }
